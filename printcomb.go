8eadc58c06fd53b12c5945ec4077a22d020b2d5a
AlexTeoKim051292 <alrkim@nu.edu.kz>
2019-10-16 23:59:47 +0600

    comb

--- printcomb.go
+++ printcomb.go
@@ -4,9 +4,9 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 
-	for x := '0'; x <= '7'; x++ {
+	for x := '0'; x <= '9'; x++ {
 
-		for y := '0'; y <= '8'; y++ {
+		for y := '0'; y <= '9'; y++ {
 
 			for z := '0'; z <= '9'; z++ {
 
