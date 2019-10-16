8da7d835ec738ed87951a229222398d5130e5ffd
AlexTeoKim051292 <alrkim@nu.edu.kz>
2019-10-16 23:57:52 +0600

    comb

---
+++ printcomb.go
@@ -0,0 +1,25 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+
+	for x := '0'; x <= '7'; i++ {
+
+		for y := '0'; y <= '8'; y++ {
+
+			for z := '0'; z <= '9'; z++ {
+
+				z01.PrintRune(x)
+				z01.PrintRune(y)
+				z01.PrintRune(z)
+				if x < '7' {
+					z01.PrintRune(44)
+					z01.PrintRune(32)
+				}
+			}
+		}
+	}
+	z01.PrintRune(10)
+
+}
