f8f425e50976604ecc3afbb02bf60e738bc186a3
AlexTeoKim051292 <alrkim@nu.edu.kz>
2019-10-19 20:30:26 +0600

    Print the word

--- printstr.go
+++ printstr.go
@@ -4,7 +4,9 @@ import "github.com/01-edu/z01"
 
 func PrintStr(str string) int {
 	var stroka = []rune(str)
+	var stroka1 string = ""
 	for i := range stroka {
-		z01.PrintRune(stroka[i])
+		stroka1 = stroka + stroka[i]
 	}
+	return stroka1
 }
