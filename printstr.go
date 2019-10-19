897175fcff3322d9fa66525bf390ad3019a4418e
AlexTeoKim051292 <alrkim@nu.edu.kz>
2019-10-19 20:43:17 +0600

    Print the word

--- printstr.go
+++ printstr.go
@@ -3,6 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
+	var stroka1: = ''
 	var stroka = []rune(str)
 	for i := range stroka {
 		stroka1 := stroka1 + stroka[i]
