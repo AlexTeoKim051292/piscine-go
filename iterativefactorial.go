6105409de10d251b208075241cbf71569dc354fb
AlexTeoKim051292 <alrkim@nu.edu.kz>
2019-10-21 20:14:14 +0600

    Iterative factorial

---
+++ iterativefactorial.go
@@ -0,0 +1,17 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+
+	result := 1
+
+	if nb <= 0 {
+		result = 0
+	} else if nb >= 15 {
+		result = 0
+	} else if nb > 0 {
+		for i := 1; i < nb + 1; i++ {
+			result = result * i
+		}
+	}
+	return result
+}
