209923a932d10fb5e81fdde45112955ceba0b9b6
AlexTeoKim051292 <alrkim@nu.edu.kz>
2019-10-23 19:56:23 +0600

    Joiner

--- join.go
+++ join.go
@@ -4,17 +4,17 @@ func Join(strs []string, sep string) string {
 
 	var result string
 	k := 0
-	for i, _ := range strs {
+	for i := range strs {
 		k = i
 	}
 
-	for i, _ := range strs {
+	for i := range strs {
 		if i != k {
 			strs[i] = strs[i] + sep
 		}
 
 	}
-	for i, _ := range strs {
+	for i := range strs {
 		result += strs[i]
 	}
 	return result
